72bab1e22b0c3196a760c7ae07dd99da57354163
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-05-04 23:40:54 +0900

    feat: footer

---
+++ client/app/Footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>2023. Early Developer</p>
+    </footer>
+  );
+};
+
+export default Footer;
