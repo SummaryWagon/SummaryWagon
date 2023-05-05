4279cb4a725a5e0060c0793a086a55786298e679
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-05-05 21:58:59 +0900

    fix: main page auth

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -18,7 +18,6 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   let session = await getServerSession(authOptions);
-  console.log(session)
   return (
     <html lang="en">
       <body className={inter.className}>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,16 +5,19 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import RealtimeSearch from "./components/RealtimeSearch";
 import History from "./components/History/History";
 import HotTopic from "./components/HotTopic/HotTopic";
-const myDummy = {};
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-export default function Home() {
+export default async function Home() {
+  let session = await getServerSession(authOptions);
+
   return (
     <main className={styles.main}>
       <SearchBar></SearchBar>
       <PopularKeywords></PopularKeywords>
       <div className={styles.categories}>
-        <History/>
-        <HotTopic/>
+        {session && <History />}
+        <HotTopic />
       </div>
       <RealtimeSearch></RealtimeSearch>
     </main>
