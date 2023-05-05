c8ef2e7251b42bce80c1260f24c8d03b4af37440
YoungwooKim09 <youngwoo9699@gmail.com>
2023-05-06 00:35:54 +0900

    feat: add cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI 
 from .routers import articles, users
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI() 
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(articles.router)
 app.include_router(users.router)
