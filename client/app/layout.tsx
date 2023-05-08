6f908ad54759ca8e57c46595605bcf611903138a
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-05-08 15:37:50 +0900

    fix: page loading UI

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         <Header session={session} />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
