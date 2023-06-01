eafbab7cc3fd5b536a6719bd84d10b1cda36581a
jamiehun <jamiesunghun@gmail.com>
2023-06-01 14:34:22 +0900

    feat:spacy_init

--- backend/app/articles/articles_service.py
+++ backend/app/articles/articles_service.py
@@ -1,10 +1,11 @@
 from . import articles_repository
 from ..users import users_repository
 
-from .articles_schema import addArticleDto
+from .articles_schema import addArticleDto, getKeywordDto
 from ..models import ResponseModel
 
-from ..utils.bs4.preprocess import word_preprocess
+from ..utils.bs4.preprocess import title_image_parsing, text_parsing
+from ..utils.bs4.keyword import keyword_finder
 from ..utils.chat_gpt.summary import summarize
 from ..utils.thumbnail.image_resizing import upload_to_s3
 
@@ -51,21 +52,12 @@ async def add_article(addArticleDto: addArticleDto):
 
         return ResponseModel(isExist, "Article already exists in DB.")
     
-    # 기사 히스토리에 추가
-    title, image, image_content_type, keyword = word_preprocess(link)
-    default_image = DEFAULT_IMAGE_URL
-
-    file_path = "app/data/articles.txt"
-    text = ''''''
-
-    f = open(file_path, "r")
-
-    while True:
-        line = f.readline()
-        if not line: break
-        text += line
-
-    summary = summarize(text)
+    # text 및 title, image 파싱
+    text_all = text_parsing(link)
+    title, image, image_content_type = title_image_parsing(link)
+    
+    # text 요약
+    summary = summarize(text_all)
 
     article = {
         "link": link,
@@ -99,3 +91,13 @@ async def add_article(addArticleDto: addArticleDto):
     print("time: ", end_time - start_time)
 
     return ResponseModel(article_id, "Article added successfully.")
+
+
+async def get_keyword(getKeywordDto: getKeywordDto):
+    link = getKeywordDto.link
+    text_all = text_parsing(link)
+    keyword = keyword_finder(text_all)
+    
+    return keyword
+    
+    
--- backend/app/utils/bs4/keyword.py
+++ backend/app/utils/bs4/keyword.py
@@ -1,23 +1,13 @@
-def find_keyword(sorted_dict):
-    
-    keyword_list = ["chatgpt", "ai", "bigdata"] # ToDo : 키워드 추가 필요
-    visited = [0] * len(keyword_list)
-    
-    for k in sorted_dict:
-        
-        for i in range(len(keyword_list)):
-            keyword = keyword_list[i]
-            length = len(keyword)
+import spacy
+nlp = spacy.load('en_core_web_sm')
 
-            # keyword_list와 keyword가 같은지 체크 (keyword_list 단어의 단어 수로 앞에서 자름)
-            if (keyword == k[0][:length].lower()):
-                visited[i] += 1
-        
-    # 최대 인덱스 값 확인 
-    frequent_kw = [0, 0] # idx, cnt 
-    for i in range(len(visited)):
-        if visited[i] > frequent_kw[1]:
-            frequent_kw = [i, visited[i]]
+def keyword_finder(text_all):
     
-    return keyword_list[frequent_kw[0]]
+    doc = nlp(text_all)
+    for sents in doc.sents:
+        print(sents.text)
+
+    return 1
+
+     
     
