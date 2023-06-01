b61c9c8d19767ad6975e7af1a05070c7a1517af6
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-06-01 22:40:17 +0900

    feat: Keyword Article

--- client/app/components/KeywordTag/KeywordTag.tsx
+++ client/app/components/KeywordTag/KeywordTag.tsx
@@ -1,33 +1,22 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./KeywordTag.module.css";
+import CardList from "../CardList";
+
 interface KeywordTagProps {
   keyword: string;
 }
 
-async function fetchData(params: any) {
-  console.log("fetch in", params);
-  console.log(`${process.env.NEXT_PUBLIC_SERVER_URL}/articles/${params}`);
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/articles/${params}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  console.log("res", res);
-  const data = await res.json();
-  console.log(data);
-  return data;
-}
-
 const dummy = ["AI", "Frontend", "Backend", "JavaScript"];
+
 const KeywordTag = () => {
   const [currentKeyword, setCurrentKeyword] = useState<string>("");
-  // const data = fetchData("6454fa571d3bfee7230729d9");
-  // console.log(data);
+
+  const onClickKeyword = (keyword: string) => {
+    console.log("key:", keyword);
+    setCurrentKeyword(keyword);
+  };
+
   return (
     <div className={styles.keywordTag}>
       <div>
@@ -35,20 +24,28 @@ const KeywordTag = () => {
       </div>
       <ul className={styles.keywordList}>
         {dummy.map((search, idx) => {
-          if (idx === 0)
-            return (
-              <li key={idx} className={styles.keywordItem}>
-                <span className={styles.keyword}>{search}</span>
-              </li>
-            );
+          const isFocused = currentKeyword === search;
           return (
-            <li key={idx} className={styles.keywordItem}>
-              <span className={styles.divide}>/</span>
-              <span className={styles.keyword}>{search}</span>
+            <li
+              key={idx}
+              className={`${styles.keywordItem}`}
+              onClick={() => {
+                onClickKeyword(search);
+              }}
+            >
+              {idx !== 0 && <span className={styles.divide}>/</span>}
+              <span
+                className={`${styles.keyword} ${
+                  isFocused ? styles.focused : ""
+                }`}
+              >
+                {search}
+              </span>
             </li>
           );
         })}
       </ul>
+      <CardList keyword={currentKeyword}></CardList>
     </div>
   );
 };
