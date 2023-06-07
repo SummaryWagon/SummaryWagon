465cec2ecffecd1d696bfeff3be562d001c07795
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-06-07 22:06:41 +0900

    fix: keyword aricle pagination ui

--- client/app/components/Pagination/index.tsx
+++ client/app/components/Pagination/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./index.module.css";
+import Image from "next/image";
+import RightArrowIcon from "@/public/icon/RightArrowIcon.svg";
+import LeftArrowIcon from "@/public/icon/LeftArrowIcon.svg";
+
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
@@ -35,8 +39,25 @@ const Pagination = ({
     return pageNumbers;
   };
 
-  return <ul className={styles.pagination}>
-    {renderPageNumbers()}</ul>;
+  return (
+    <ul className={styles.pagination}>
+      <Image
+        className={styles.leftArrowIcon}
+        src={LeftArrowIcon}
+        height={15}
+        width={15}
+        alt=""
+      ></Image>
+      {renderPageNumbers()}
+      <Image
+        className={styles.rightArrowIcon}
+        src={RightArrowIcon}
+        height={15}
+        width={15}
+        alt=""
+      ></Image>
+    </ul>
+  );
 };
 
 export default Pagination;
