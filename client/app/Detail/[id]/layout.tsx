005945b8a9e972351ad1e7082744343ff908bf94
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-06-09 17:50:16 +0900

    repactor: detail page

--- client/app/Detail/[id]/layout.tsx
+++ client/app/Detail/[id]/layout.tsx
@@ -1,11 +1,8 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
