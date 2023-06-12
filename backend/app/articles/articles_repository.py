b3476e34f1d75c4678176cfb036fb9c2f6985aa9
YoungwooKim09 <youngwoo9699@gmail.com>
2023-06-12 23:52:15 +0900

    feat: next page 유무, pagination 쿼리 수정

--- backend/app/articles/articles_repository.py
+++ backend/app/articles/articles_repository.py
@@ -1,6 +1,7 @@
 from ..database import db
 from bson.objectid import ObjectId
 from .articles_schema import Article
+from datetime import datetime
 
 
 def article_helper(articles):
@@ -18,9 +19,11 @@ async def find_articles(article_ids: list[str]):
 
 async def find_articles_by_category(keyword: str, page: int, limit: int):
     offset = (page - 1) * limit
-    articles = await db.articles.find({"categories": keyword}).sort([("cnt", -1), ("_id", -1)]).skip(offset).limit(limit).to_list(length=limit)
 
-    return article_helper(articles)
+    articles = await db.articles.find({"categories": keyword}).sort([("cnt", -1), ("_id", -1)]).skip(offset).limit(limit).to_list(length=limit)
+    total = await db.articles.count_documents({"categories": keyword})
+
+    return {"total": total, "articles": article_helper(articles)}
 
 
 async def find_all_articles(article_ids: list[str], limit: int, next: str | None):
@@ -28,7 +31,8 @@ async def find_all_articles(article_ids: list[str], limit: int, next: str | None
         articles = await db.articles.find({"_id": {"$in": [ObjectId(article_id) for article_id in article_ids]}}).sort([("datetime", -1), ("_id", -1)]).to_list(length=limit)
     else:
         next_datetime, next_id = next.split("_")
-        articles = await db.articles.find({"$or": [{"datetime": {"$lt": next_datetime}}, {"_id": {"$lt": ObjectId(next_id)}}]}).sort([("datetime", -1), ("_id", -1)]).to_list(length=limit)
+        next_datetime_format = datetime.fromisoformat(next_datetime)
+        articles = await db.articles.find({"$or": [{"datetime": {"$lt": next_datetime_format}}, {"datetime": next_datetime_format, "_id": {"$lt": ObjectId(next_id)}}]}).sort([("datetime", -1), ("_id", -1)]).to_list(length=limit)
 
     return article_helper(articles)
 
@@ -44,9 +48,33 @@ async def find_all_hot_articles(limit: int, next: str | None):
         articles = await db.articles.find().sort([("cnt", -1), ("_id", -1)]).to_list(length=limit)
     else:
         next_cnt, next_id = next.split("_")
-        articles = await db.articles.find({"$or": [{"cnt": {"$lt": next_cnt}}, {"_id": {"$lt": ObjectId(next_id)}}]}).sort([("cnt", -1), ("_id", -1)]).to_list(length=limit)
-    
+        next_cnt_format = int(next_cnt)
+        articles = await db.articles.find({"$or": [{"cnt": {"$lt": next_cnt_format}}, {"cnt": next_cnt_format, "_id": {"$lt": ObjectId(next_id)}}]}).sort([("cnt", -1), ("_id", -1)]).to_list(length=limit)
+        
     return article_helper(articles)
+
+
+async def has_next(param: str, article):
+    if param == "datetime":
+        q = {"$or": [{"datetime": {"$lt": article["datetime"]}}, {"datetime": article["datetime"], "_id": {"$lt": ObjectId(article["_id"])}}]}
+    elif param == "cnt":
+        q = {"$or": [{"cnt": {"$lt": article["cnt"]}}, {"cnt": article["cnt"], "_id": {"$lt": ObjectId(article["_id"])}}]}
+    
+    r = await db.articles.find_one(q)
+    
+    if r is None:
+        return False
+    
+    return True
+
+
+async def find_keyword():
+    keywordList = []
+
+    async for category in db.articles.find({}, {"_id": 0, "categories": 1}):
+        keywordList.append(category["categories"])
+
+    return keywordList
 
 
 async def find_article(article_id: str):
@@ -76,12 +104,3 @@ async def update_article(article_id: str, image_url: str):
 
 async def update_article_cnt(article_id: str):
     return await db.articles.update_one({"_id": ObjectId(article_id)}, {"$inc": {"cnt": 1}})
-
-
-async def find_keyword():
-    keywordList = []
-
-    async for category in db.articles.find({}, {"_id": 0, "categories": 1}):
-        keywordList.append(category["categories"])
-
-    return keywordList
