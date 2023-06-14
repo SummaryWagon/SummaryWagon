2023f9dea4a2e7c4001e850964d3fc0126c0965b
YoungwooKim09 <youngwoo9699@gmail.com>
2023-06-14 22:18:23 +0900

    feat: 문장 경계 처리 & 최대 토큰 시 문장 단위 분할

--- backend/app/utils/chat_gpt/count.py
+++ backend/app/utils/chat_gpt/count.py
@@ -1,7 +1,26 @@
-import tiktoken
+import tiktoken, nltk
 
-def num_tokens_from_string(string: str, model: str) -> int:
+maximum_token_length = 3895 # 4096 - 200 - 1
+
+
+def split_prompt(string: str, model: str) -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.encoding_for_model(model)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
+    sentences = nltk.sent_tokenize(string)
+    
+    num_tokens = 0
+    next = 0
+    prompt_chunks = []
+
+    for i in range(len(sentences)):
+        num_tokens += len(encoding.encode(sentences[i]))
+
+        if num_tokens > maximum_token_length:
+            prompt_chunks.append(" ".join(sentences[:i]))
+            num_tokens = len(encoding.encode(sentences[i]))
+            next = i
+
+        if i == len(sentences) - 1:
+            prompt_chunks.append(" ".join(sentences[next:]))
+    
+    return prompt_chunks
