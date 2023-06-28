a2cd2794073eb6713893a290c15a0769f74fd94e
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-06-28 14:47:48 +0900

    fix: metadata

--- client/app/hot/page.tsx
+++ client/app/hot/page.tsx
@@ -1,15 +1,36 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { clientDB } from "@/util/database";
 import ArticleList from "../components/ArticleList";
+import RightArrowIcon from "@/public/icon/RightArrowIcon.svg";
+import HotArticleList from "../components/HotArticleList";
+import { Metadata } from "next";
+
+
 export const dynamic = "force-dynamic";
 
-export default async function Hot() {
-  // let db = (await clientDB).db("dbEarlyDev");
-  // let result = await db.collection("articles").find({}).toArray();
-  return (
-    <main className={styles.main}>
-      <ArticleList></ArticleList>
-    </main>
+export const generateMetadata = async ({ params }: any): Promise<Metadata> => {
+  return {
+    title: '트렌드 토픽',
+    description: 'SummaryWagon에서 가장 인기있는 토픽을 확인해보세요!',
+  };
+};
+
+async function fetchData() {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${10}&offset=${10 * 2}`,
+    {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
   );
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+export default async function Hot() {
+  return <main className={styles.main}>{<HotArticleList />}</main>;
 }
